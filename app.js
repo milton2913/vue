dd7911dfd909311823572ce2ce59afe6d1835e2e
Milton <web3@daffodilvarsity.edu.bd>
2021-10-23 12:22:48 +0600

    event hadle

--- app.js
+++ app.js
@@ -9,17 +9,35 @@ const app = Vue.createApp({
             author:'Kamal',
             age:26,
             c:0,
-            bookShow:true
+            bookShow:true,
+            x:0,
+            y:0
         }
     },
     methods:{
-    //     changeTitle(title){
-    //         this.title="Bangladesh big bigtory at 1971 "
-    // }
+        changeTitle(title){
+            this.title="Bangladesh big bigtory at 1971 "
+        },
 
-showBooksToggle(){
-    this.bookShow=!this.bookShow
-}
+        showBooksToggle(){
+            this.bookShow=!this.bookShow
+        },
+        handelEvent(e, data){
+            console.log(e,e.type)
+            if(data){
+                console.log(data)
+            }
+        },
+        handelMouseMove(e){
+            this.x = e.offsetX
+            this.y = e.offsetY
+        },
+        deptEdit(data){
+            if(data==1){
+               console.log('dhaka')
+            }
+            console.log(data)
+        }
 }
 })
 
